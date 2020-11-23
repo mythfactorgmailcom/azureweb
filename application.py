7967e80969b9a2193a26fd1e48f586f45adfbafb
mythfactorgmailcom <63692394+mythfactorgmailcom@users.noreply.github.com>
2020-11-23 16:43:08 +0545

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
- from flask import Flask
- app = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
 
- @app.route("/")
- def hello():
+@app.route("/")
+def hello():
     return "<h1>Hello World!</h1>"
-  
- if __name__ == '__main__':
-   app.run()
+
+if __name__ == '__main__':
+    app.run()
